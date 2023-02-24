153c1eb74b99963b9bb2ac5dd2c1a84c6fd0baa7
yoshilarkins <yoshilarkins@gmail.com>
2023-02-23 20:50:21 -0500

    Dusting off crayon box

--- Crayon eaters/crayon_box.m
+++ Crayon eaters/crayon_box.m
@@ -8,16 +8,21 @@ throttleX = 0:1/STEPS:1;
 throttleCurve = polyval([1 0], throttleX);
 
 % Degrees C, Max Amps
-motorControllerCurve = [25, 130;
-                        50, 65;
+%This curve does not consider that there are two motor controllers
+%Changing program to take max of both controller temps 
+% and mapping to 2 times this curve
+% motorControllerCurve = [25, 130;
+%                         50, 65;
+%                         75, 0];
+motorControllerCurve = [25, 260;
+                        50, 130;
                         75, 0];
 
 %Parabolic function
-maxMotorTemp = 100;
-motorCoef = 1;
-motorTempX = 0 : maxMotorTemp / STEPS : maxMotorTemp;
-motorTempCurve = (1 - motorCoef * ...
-    (motorTempX / maxMotorTemp) .^ 2);
+maxMotorTemp = 95;
+motorTempX = minMotorTemp : (maxMotorTemp-minMotorTemp) / STEPS : maxMotorTemp;
+motorTempCurve = 140 -  (motorTempX - minMotorTemp) .^ 2 .* (140) / (maxMotorTemp - minMotorTemp)^2;
+
 
 %{
 maxControlTemp = 100;
@@ -26,9 +31,9 @@ controlCoef = 1;
 controlTempCurve = (1 - controlCoef * ...
     (controlTempX / maxControlTemp) .^ 2);
 %}
-
-maxBatteryTemp = 60;
-batteryCoef = 1;
-batteryTempX = 0 : maxBatteryTemp / STEPS : maxBatteryTemp;
-batteryTempCurve = (1 - batteryCoef * ...
-    (batteryTempX / maxBatteryTemp) .^ 2);
+% 
+% maxBatteryTemp = 60;
+% batteryCoef = 1;
+% batteryTempX = 0 : maxBatteryTemp / STEPS : maxBatteryTemp;
+% batteryTempCurve = (1 - batteryCoef * ...
+%     (batteryTempX / maxBatteryTemp) .^ 2);
